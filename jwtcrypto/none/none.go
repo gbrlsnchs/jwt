186fe14e56aaa64b1cf7254b7f6d52b857a89afc
Gabriel Sanches <gsanches.dev@gmail.com>
2018-02-06 17:35:29 -0200

    Add documentation for None

--- jwtcrypto/none/none.go
+++ jwtcrypto/none/none.go
@@ -2,12 +2,17 @@ package none
 
 import "github.com/gbrlsnchs/jwt/jwtcrypto"
 
+// None is a dull signing method that
+// signs a message as is and is always valid.
 type None struct{}
 
+// HasKey returns false, since None
+// doesn't make use of any keys.
 func (n *None) HasKey() bool {
 	return false
 }
 
+// Sign simply returns a message as is.
 func (n *None) Sign(digest []byte) ([]byte, error) {
 	return digest, nil
 }
@@ -16,6 +21,7 @@ func (n *None) String() jwtcrypto.SigningMethod {
 	return jwtcrypto.None
 }
 
+// Verify returns true, and thus None is always valid.
 func (n *None) Verify(_, _ []byte) (bool, error) {
 	return true, nil
 }
