bb4607615792d02412393be40708d21e17ed3f0a
Gabriel Sanches <gsanches.dev@gmail.com>
2018-02-16 13:14:42 -0200

    Change JWTIDValidator to IDValidator to fit better under the package's name

--- validators.go
+++ validators.go
@@ -10,12 +10,13 @@ var (
 	ErrAudienceMismatch  = errors.New("jwt: Audience mismatch")
 	ErrTokenExpired      = errors.New("jwt: Token expired")
 	ErrTokenFromFuture   = errors.New("jwt: Token issued at the future")
-	ErrPrematureToken    = errors.New("jwt: Token is not valid yet")
+	ErrTokenTooYoung     = errors.New("jwt: Token is not valid yet")
 	ErrIssuerMismatch    = errors.New("jwt: Issuer mismatch")
 	ErrJWTIDMismatch     = errors.New("jwt: JWTID mismatch")
 	ErrSubjectMismatch   = errors.New("jwt: Subject mismatch")
 )
 
+// AlgorithmValidator validates the "alg" claim.
 func AlgorithmValidator(alg string) ValidatorFunc {
 	return func(jot *JWT) error {
 		if alg != jot.Algorithm() {
@@ -26,6 +27,7 @@ func AlgorithmValidator(alg string) ValidatorFunc {
 	}
 }
 
+// AudienceValidator validates the "aud" claim.
 func AudienceValidator(aud string) ValidatorFunc {
 	return func(jot *JWT) error {
 		if jot.Audience() != aud {
@@ -36,6 +38,7 @@ func AudienceValidator(aud string) ValidatorFunc {
 	}
 }
 
+// ExpirationTimeValidator validates the "exp" claim.
 func ExpirationTimeValidator(now time.Time) ValidatorFunc {
 	return func(jot *JWT) error {
 		if exp := jot.ExpirationTime(); !exp.IsZero() && now.After(exp) {
@@ -46,6 +49,7 @@ func ExpirationTimeValidator(now time.Time) ValidatorFunc {
 	}
 }
 
+// IssuedAtValidator validates the "iat" claim.
 func IssuedAtValidator(now time.Time) ValidatorFunc {
 	return func(jot *JWT) error {
 		if now.Before(jot.IssuedAt()) {
@@ -56,6 +60,7 @@ func IssuedAtValidator(now time.Time) ValidatorFunc {
 	}
 }
 
+// IssuerValidator validates the "iss" claim.
 func IssuerValidator(iss string) ValidatorFunc {
 	return func(jot *JWT) error {
 		if jot.Issuer() != iss {
@@ -66,17 +71,8 @@ func IssuerValidator(iss string) ValidatorFunc {
 	}
 }
 
-func NotBeforeValidator(now time.Time) ValidatorFunc {
-	return func(jot *JWT) error {
-		if now.Before(jot.NotBefore()) {
-			return ErrPrematureToken
-		}
-
-		return nil
-	}
-}
-
-func JWTIDValidator(jti string) ValidatorFunc {
+// IDValidator validates the "jti" claim.
+func IDValidator(jti string) ValidatorFunc {
 	return func(jot *JWT) error {
 		if jot.ID() != jti {
 			return ErrJWTIDMismatch
@@ -86,6 +82,18 @@ func JWTIDValidator(jti string) ValidatorFunc {
 	}
 }
 
+// NotBeforeValidator validates the "nbf" claim.
+func NotBeforeValidator(now time.Time) ValidatorFunc {
+	return func(jot *JWT) error {
+		if now.Before(jot.NotBefore()) {
+			return ErrTokenTooYoung
+		}
+
+		return nil
+	}
+}
+
+// SubjectValidator validates the "sub" claim.
 func SubjectValidator(sub string) ValidatorFunc {
 	return func(jot *JWT) error {
 		if jot.Subject() != sub {
