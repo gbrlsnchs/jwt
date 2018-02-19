85c313df74855786efafc673702be7e0a451b421
Gabriel Sanches <gsanches.dev@gmail.com>
2018-02-19 19:26:09 -0300

    Move mock-ups and testing helpers to internal package

---
+++ internal/jwt_mockup.go
@@ -0,0 +1,3 @@
+package internal
+
+const JWTMockup = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ"
