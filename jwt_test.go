4f6ca8fd2006ef481186066d1a487de4fa375aad
Gabriel Sanches <gsanches.dev@gmail.com>
2018-08-22 18:23:44 -0300

    Refactor "FromContext" and "FromCookie" tests

--- jwt_test.go
+++ jwt_test.go
@@ -8,10 +8,8 @@ import (
 	"testing"
 
 	. "github.com/gbrlsnchs/jwt"
-	. "github.com/gbrlsnchs/jwt/internal"
 )
 
-const key = byte(0)
 const mock = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.
 eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.
 TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ`
@@ -21,48 +19,31 @@ const mockMalformed = `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.
 eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9`
 
 func TestFromContext(t *testing.T) {
+	const key = byte(0) // cheap key
 	testCases := []struct {
-		jot interface{}
-		key interface{}
-		err error
+		jot     interface{}
+		keyless bool
+		err     error
 	}{
-		{
-			err: ErrNilCtxKey,
-		},
-		{
-			key: key,
-			jot: 1,
-			err: ErrCtxAssertion,
-		},
-		{
-			key: key,
-			err: ErrNilCtxValue,
-		},
-		{
-			key: key,
-			jot: mock,
-		},
-		{
-			key: key,
-			jot: mockNone,
-		},
-		{
-			key: key,
-			jot: mockMalformed,
-			err: ErrMalformedToken,
-		},
-		{
-			key: key,
-			jot: &JWT{},
-		},
+		{keyless: true, err: ErrNilCtxKey},
+		{jot: 1, err: ErrCtxAssertion},
+		{err: ErrNilCtxValue},
+		{jot: "", err: ErrMalformedToken},
+		{jot: mock},
+		{jot: mockNone},
+		{jot: mockMalformed, err: ErrMalformedToken},
+		{jot: &JWT{}},
 	}
 	for _, tc := range testCases {
 		t.Run("", func(t *testing.T) {
 			ctx := context.Background()
-			if tc.key != nil {
-				ctx = context.WithValue(ctx, tc.key, tc.jot)
+			var k interface{} = key
+			if !tc.keyless {
+				ctx = context.WithValue(ctx, k, tc.jot)
+			} else {
+				k = nil
 			}
-			_, err := FromContext(ctx, tc.key)
+			_, err := FromContext(ctx, k)
 			if want, got := tc.err, err; want != got {
 				t.Errorf("want %v, got %v", want, got)
 			}
@@ -71,18 +52,24 @@ func TestFromContext(t *testing.T) {
 }
 
 func TestFromCookie(t *testing.T) {
-	c := &http.Cookie{Name: "test"}
-	_, err := FromCookie(c)
-
-	if want, got := ErrMalformedToken, err; want != got {
-		errorf(t, want, got)
+	const name = "doge"
+	testCases := []struct {
+		jot string
+		err error
+	}{
+		{jot: "", err: ErrMalformedToken},
+		{jot: mock},
+		{jot: mockNone},
+		{jot: mockMalformed, err: ErrMalformedToken},
 	}
-
-	c.Value = JWTMockup
-	_, err = FromCookie(c)
-
-	if want, got := (error)(nil), err; want != got {
-		errorf(t, want, got)
+	for _, tc := range testCases {
+		t.Run("", func(t *testing.T) {
+			c := &http.Cookie{Value: tc.jot}
+			_, err := FromCookie(c)
+			if want, got := tc.err, err; want != got {
+				t.Errorf("want %v, got %v", want, got)
+			}
+		})
 	}
 }
 
