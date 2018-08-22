14766efc995bb42cf2526f265149259594a4bbb4
Gabriel Sanches <gsanches.dev@gmail.com>
2018-08-22 19:37:47 -0300

    Delete internal package

--- internal/jwt_mockup.go
+++
@@ -1,3 +0,0 @@
-package internal
-
-const JWTMockup = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiYWRtaW4iOnRydWV9.TJVA95OrM7E2cBab30RMHrHDcEfxjoYZgeFONFh7HgQ"
