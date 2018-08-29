94ed0532bc6e967db5a07b94d160ef3531694270
Gabriel Sanches <gsanches.dev@gmail.com>
2018-08-29 19:05:27 -0300

    Add benchmark tests

--- benchmark_test.go
+++ benchmark_test.go
@@ -7,27 +7,41 @@ import (
 	. "github.com/gbrlsnchs/jwt"
 )
 
-const benchMock = `eyJhbGciOiJIUzI1NiIsImtpZCI6Im15X2tleSIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhZG1pbiIsImV4cCI6MTU2NjY1MzM3NywiZm9vIjoiYmFyIiwiaWF0IjoxNTM1NTQ5Mzc3LCJpc3MiOiJhdXRoX3NlcnZlciIsImp0aSI6InVuaXF1ZV9pZCIsIm15Qm9vbCI6dHJ1ZSwibmJmIjoxNTM1NTUxMTc3LCJzdWIiOiIxMjMifQ.ktYQOGGGYoExeH86Oh6sjTefiMSllYuBsdVE4dwwNiI`
+var (
+	benchMock   = []byte("eyJhbGciOiJIUzI1NiIsImtpZCI6Im15X2tleSIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhZG1pbiIsImV4cCI6MTU2NjY1MzM3NywiZm9vIjoiYmFyIiwiaWF0IjoxNTM1NTQ5Mzc3LCJpc3MiOiJhdXRoX3NlcnZlciIsImp0aSI6InVuaXF1ZV9pZCIsIm15Qm9vbCI6dHJ1ZSwibmJmIjoxNTM1NTUxMTc3LCJzdWIiOiIxMjMifQ.ktYQOGGGYoExeH86Oh6sjTefiMSllYuBsdVE4dwwNiI")
+	benchSigner = HS256("b3nchm4rk")
+)
 
-var benchSigner = HS256("b3nchm4rk")
+type Token struct {
+	*JWT
+	Foo  string `json:"foo,omitempty"`
+	Bool bool   `json:"bool,omitempty"`
+}
 
 func BenchmarkSign(b *testing.B) {
 	now := time.Now()
-	opt := &Options{
-		JWTID:          "unique_id",
-		Timestamp:      true,
-		ExpirationTime: now.Add(24 * 30 * 12 * time.Hour),
-		NotBefore:      now.Add(30 * time.Minute),
-		Subject:        "123",
-		Audience:       "admin",
-		Issuer:         "auth_server",
-		KeyID:          "my_key",
-		Public:         map[string]interface{}{"foo": "bar", "myBool": true},
+	token := &Token{
+		JWT: &JWT{
+			Header: &Header{
+				Algorithm: benchSigner.String(),
+				KeyID:     "my_key",
+			},
+			Claims: &Claims{
+				ID:         "unique_id",
+				IssuedAt:   now,
+				Expiration: now.Add(24 * 30 * 12 * time.Hour),
+				NotBefore:  now.Add(30 * time.Minute),
+				Subject:    "123",
+				Audience:   "admin",
+				Issuer:     "auth_server",
+			},
+		},
+		Foo:  "bar",
+		Bool: true,
 	}
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		_, err := Sign(benchSigner, opt)
-		if err != nil {
+		if _, err := benchSigner.Sign(token); err != nil {
 			b.Error(err)
 		}
 	}
@@ -35,11 +49,8 @@ func BenchmarkSign(b *testing.B) {
 
 func BenchmarkVerify(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		jot, err := FromString(benchMock)
-		if err != nil {
-			b.Error(err)
-		}
-		if err = jot.Verify(benchSigner); err != nil {
+		var token Token
+		if err := benchSigner.Verify(benchMock, &token); err != nil {
 			b.Error(err)
 		}
 	}
