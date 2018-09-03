c366e7ac6dc0d4630361c9a0214a9b958c13c1ed
Gabriel Sanches <gsanches.dev@gmail.com>
2018-09-03 14:44:47 -0300

    Update HMAC tests

--- hmac_test.go
+++ hmac_test.go
@@ -10,16 +10,16 @@ func TestHMAC(t *testing.T) {
 	testCases := []testCase{
 		{NewHS256(""), NewHS256(""), nil, ErrNoHMACKey, nil, nil, ErrNoHMACKey},
 		{NewHS256("secret"), NewHS256("secret"), nil, nil, nil, nil, nil},
-		{NewHS256("secret"), NewHS256("not_secret"), nil, nil, nil, nil, ErrHMACInvalid},
-		{NewHS256("not_secret"), NewHS256("secret"), nil, nil, nil, nil, ErrHMACInvalid},
+		{NewHS256("secret"), NewHS256("not_secret"), nil, nil, nil, nil, ErrHMACVerification},
+		{NewHS256("not_secret"), NewHS256("secret"), nil, nil, nil, nil, ErrHMACVerification},
 		{NewHS384(""), NewHS384(""), nil, ErrNoHMACKey, nil, nil, ErrNoHMACKey},
 		{NewHS384("secret"), NewHS384("secret"), nil, nil, nil, nil, nil},
-		{NewHS384("secret"), NewHS384("not_secret"), nil, nil, nil, nil, ErrHMACInvalid},
-		{NewHS384("not_secret"), NewHS384("secret"), nil, nil, nil, nil, ErrHMACInvalid},
+		{NewHS384("secret"), NewHS384("not_secret"), nil, nil, nil, nil, ErrHMACVerification},
+		{NewHS384("not_secret"), NewHS384("secret"), nil, nil, nil, nil, ErrHMACVerification},
 		{NewHS512(""), NewHS512(""), nil, ErrNoHMACKey, nil, nil, ErrNoHMACKey},
 		{NewHS512("secret"), NewHS512("secret"), nil, nil, nil, nil, nil},
-		{NewHS512("secret"), NewHS512("not_secret"), nil, nil, nil, nil, ErrHMACInvalid},
-		{NewHS512("not_secret"), NewHS512("secret"), nil, nil, nil, nil, ErrHMACInvalid},
+		{NewHS512("secret"), NewHS512("not_secret"), nil, nil, nil, nil, ErrHMACVerification},
+		{NewHS512("not_secret"), NewHS512("secret"), nil, nil, nil, nil, ErrHMACVerification},
 	}
 	testJWT(t, testCases)
 }
