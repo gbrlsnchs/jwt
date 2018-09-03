7e3c658030cee0ec0254d03661f705f279f41748
Gabriel Sanches <gsanches.dev@gmail.com>
2018-09-03 20:14:32 -0300

    Add tests for validators

---
+++ validators_test.go
@@ -0,0 +1,59 @@
+package jwt_test
+
+import (
+	"reflect"
+	"runtime"
+	"strings"
+	"testing"
+	"time"
+
+	. "github.com/gbrlsnchs/jwt/v2"
+)
+
+func TestValidators(t *testing.T) {
+	var now time.Time
+	jot := &JWT{
+		Claims: &Claims{
+			IssuedAt:   now.Unix(),
+			Expiration: now.Add(24 * time.Hour).Unix(),
+			NotBefore:  now.Add(15 * time.Second).Unix(),
+			ID:         "jti",
+			Audience:   "aud",
+			Subject:    "sub",
+			Issuer:     "iss",
+		},
+	}
+	testCases := []struct {
+		validator ValidatorFunc
+		err       error
+	}{
+		{IssuedAtValidator(now), nil},
+		{IssuedAtValidator(time.Unix(now.Unix()+1, 0)), nil},
+		{IssuedAtValidator(time.Unix(now.Unix()-1, 0)), ErrIatValidation},
+		{ExpirationValidator(now), nil},
+		{ExpirationValidator(time.Unix(now.Unix()-int64(24*time.Hour), 0)), nil},
+		{ExpirationValidator(time.Unix(now.Unix()+int64(24*time.Hour), 0)), ErrExpValidation},
+		{NotBeforeValidator(now), ErrNbfValidation},
+		{NotBeforeValidator(time.Unix(now.Unix()+int64(15*time.Second), 0)), nil},
+		{NotBeforeValidator(time.Unix(now.Unix()-int64(15*time.Second), 0)), ErrNbfValidation},
+		{IDValidator("jti"), nil},
+		{IDValidator("not_jti"), ErrJtiValidation},
+		{AudienceValidator("aud"), nil},
+		{AudienceValidator("not_aud"), ErrAudValidation},
+		{SubjectValidator("sub"), nil},
+		{SubjectValidator("not_sub"), ErrSubValidation},
+		{IssuerValidator("iss"), nil},
+		{IssuerValidator("not_iss"), ErrIssValidation},
+	}
+	for _, tc := range testCases {
+		fn := runtime.FuncForPC(reflect.ValueOf(tc.validator).Pointer())
+		name := fn.Name()[:]
+		name = strings.TrimPrefix(name, "github.com/gbrlsnchs/jwt/v2.")
+		name = strings.TrimSuffix(name, ".func1")
+		t.Run(name, func(t *testing.T) {
+			if want, got := tc.err, tc.validator(jot); want != got {
+				t.Errorf("want %v, got %v", want, got)
+			}
+		})
+	}
+}
