9b744cf09711a11562158c5e08c9b6b535fb5ce9
Gabriel Sanches <gsanches.dev@gmail.com>
2018-09-02 21:35:33 -0300

    Change Signer interface signature

--- signer.go
+++ signer.go
@@ -1,7 +1,9 @@
 package jwt
 
+// Signer is a signing method capable of
+// both signing and verifying a JWT.
 type Signer interface {
-	Sign(Marshaler) ([]byte, error)
-	Verify([]byte, Marshaler) error
-	String() string
+	Sign([]byte) ([]byte, error)
+	Verify([]byte, []byte) error
+	String() string // prints a specific text used in the "alg" field
 }
