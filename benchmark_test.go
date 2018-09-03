8c2d56ad55837ea29253473709f82160cc06c9b6
Gabriel Sanches <gsanches.dev@gmail.com>
2018-09-02 23:45:18 -0300

    Organize benchmarks

--- benchmark_test.go
+++ benchmark_test.go
@@ -7,51 +7,61 @@ import (
 	. "github.com/gbrlsnchs/jwt"
 )
 
-var (
-	benchMock   = []byte("eyJhbGciOiJIUzI1NiIsImtpZCI6Im15X2tleSIsInR5cCI6IkpXVCJ9.eyJhdWQiOiJhZG1pbiIsImV4cCI6MTU2NjY1MzM3NywiZm9vIjoiYmFyIiwiaWF0IjoxNTM1NTQ5Mzc3LCJpc3MiOiJhdXRoX3NlcnZlciIsImp0aSI6InVuaXF1ZV9pZCIsIm15Qm9vbCI6dHJ1ZSwibmJmIjoxNTM1NTUxMTc3LCJzdWIiOiIxMjMifQ.ktYQOGGGYoExeH86Oh6sjTefiMSllYuBsdVE4dwwNiI")
-	benchSigner = HS256("b3nchm4rk")
-)
+const benchMock = "eyJhbGciOiJIUzI1NiIsImtpZCI6ImtpZCIsInR5cCI6IkpXVCJ9.eyJpYXQiOjE1MzU5NDE1NjcsImV4cCI6MTU2NzA0NTU2NywibmJmIjoxNTM1OTQzMzY3LCJqdGkiOiJCZW5jaG1hcmtTaWduIiwiYXVkIjoiYmVuY2htYXJrIiwic3ViIjoibWUiLCJpc3MiOiJnYnJsc25jaHMiLCJuYW1lIjoiZm9vYmFyIiwiaXNCZW5jaCI6dHJ1ZX0.bJSm0om7-BRCuLbICllYEAH7YsAT1cW2fdSfKcMnhOg"
 
-type Token struct {
+var benchSigner = HS256("benchmark")
+
+type benchToken struct {
 	*JWT
-	Foo  string `json:"foo,omitempty"`
-	Bool bool   `json:"bool,omitempty"`
+	Name    string `json:"name,omitempty"`
+	IsBench bool   `json:"isBench"`
 }
 
 func BenchmarkSign(b *testing.B) {
 	now := time.Now()
-	token := &Token{
+	jot := &benchToken{
 		JWT: &JWT{
 			Header: &Header{
 				Algorithm: benchSigner.String(),
-				KeyID:     "my_key",
+				KeyID:     "kid",
 			},
 			Claims: &Claims{
-				ID:         "unique_id",
-				IssuedAt:   now,
-				Expiration: now.Add(24 * 30 * 12 * time.Hour),
-				NotBefore:  now.Add(30 * time.Minute),
-				Subject:    "123",
-				Audience:   "admin",
-				Issuer:     "auth_server",
+				ID:         b.Name(),
+				IssuedAt:   now.Unix(),
+				Expiration: now.Add(24 * 30 * 12 * time.Hour).Unix(),
+				NotBefore:  now.Add(30 * time.Minute).Unix(),
+				Subject:    "me",
+				Audience:   "benchmark",
+				Issuer:     "gbrlsnchs",
 			},
 		},
-		Foo:  "bar",
-		Bool: true,
+		Name:    "foobar",
+		IsBench: true,
 	}
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
-		if _, err := benchSigner.Sign(token); err != nil {
-			b.Error(err)
+		payload, err := Marshal(jot)
+		if err != nil {
+			b.Fatal(err)
+		}
+		if _, err = benchSigner.Sign(payload); err != nil {
+			b.Fatal(err)
 		}
 	}
 }
 
 func BenchmarkVerify(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		var token Token
-		if err := benchSigner.Verify(benchMock, &token); err != nil {
-			b.Error(err)
+		payload, sig, err := Parse(benchMock)
+		if err != nil {
+			b.Fatal(err)
+		}
+		var jot benchToken
+		if err = Unmarshal(payload, &jot); err != nil {
+			b.Fatal(err)
+		}
+		if err = benchSigner.Verify(payload, sig); err != nil {
+			b.Fatal(err)
 		}
 	}
 }
