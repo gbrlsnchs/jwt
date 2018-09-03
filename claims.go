28873e2dd6a598f7d74b816b7668149b8ad30528
Gabriel Sanches <gsanches.dev@gmail.com>
2018-09-02 21:34:26 -0300

    Use unix time (int64) instead of Time structs

--- claims.go
+++ claims.go
@@ -1,22 +1,11 @@
 package jwt
 
-import (
-	"time"
-)
-
 type Claims struct {
-	*claims              // necessary hack until Go omits zero value structs
-	IssuedAt   time.Time `json:"-"`
-	Expiration time.Time `json:"-"`
-	NotBefore  time.Time `json:"-"`
-	ID         string    `json:"jti,omitempty"`
-	Audience   string    `json:"aud,omitempty"`
-	Subject    string    `json:"sub,omitempty"`
-	Issuer     string    `json:"iss,omitempty"`
-}
-
-type claims struct {
-	Iat int64 `json:"iat,omitempty"`
-	Exp int64 `json:"exp,omitempty"`
-	Nbf int64 `json:"nbf,omitempty"`
+	IssuedAt   int64  `json:"iat,omitempty"`
+	Expiration int64  `json:"exp,omitempty"`
+	NotBefore  int64  `json:"nbf,omitempty"`
+	ID         string `json:"jti,omitempty"`
+	Audience   string `json:"aud,omitempty"`
+	Subject    string `json:"sub,omitempty"`
+	Issuer     string `json:"iss,omitempty"`
 }
