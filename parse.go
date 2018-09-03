d92509ac25e029647d870268238bc18fa421d072
Gabriel Sanches <gsanches.dev@gmail.com>
2018-09-02 21:36:23 -0300

    Add a string parser function

--- parse.go
+++ parse.go
@@ -1,20 +1,24 @@
 package jwt
 
-import (
-	"bytes"
-)
+import "bytes"
 
-func parseBytes(b []byte) ([]byte, []byte, error) {
-	sep1 := bytes.IndexByte(b, '.')
+// Parse splits a string JWT representation in payload and signature for further inspection.
+func Parse(token string) ([]byte, []byte, error) {
+	return ParseBytes([]byte(token))
+}
+
+// ParseBytes does the same parsing as Parse but accepts a byte slice instead.
+func ParseBytes(token []byte) ([]byte, []byte, error) {
+	sep1 := bytes.IndexByte(token, '.')
 	if sep1 < 0 { // RFC 7519, section 7.2.1
 		return nil, nil, ErrMalformedToken
 	}
 
-	cls := b[sep1+1:]
+	cls := token[sep1+1:]
 	sep2 := bytes.IndexByte(cls, '.')
 	if sep2 < 0 {
 		return nil, nil, ErrMalformedToken
 	}
 	sep2 += sep1 + 1
-	return b[:sep2], b[sep2+1:], nil
+	return token[:sep2], token[sep2+1:], nil
 }
