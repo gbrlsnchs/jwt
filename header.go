7240c9bcb96f09aab36fad73d731cc81eb57a388
Gabriel Sanches <g@lonemaker.com>
2018-09-04 17:40:18 -0300

    Remove cty header parameter

--- header.go
+++ header.go
@@ -4,8 +4,7 @@ package jwt
 //
 // Parameters are ordered according to the RFC 7515.
 type header struct {
-	Algorithm   string `json:"alg,omitempty"`
-	KeyID       string `json:"kid,omitempty"`
-	Type        string `json:"typ,omitempty"`
-	ContentType string `json:"cty,omitempty"` // TODO: implement JWT nesting
+	Algorithm string `json:"alg,omitempty"`
+	KeyID     string `json:"kid,omitempty"`
+	Type      string `json:"typ,omitempty"`
 }
