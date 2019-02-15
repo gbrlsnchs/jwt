aad5d7d64ea2b545137d95a087ef1f655f9cb15a
Gabriel Sanches <g@lonemaker.com>
2019-02-15 15:02:21 -0200

    Compact JWT type

---
+++ claims.go
@@ -0,0 +1,11 @@
+package jwt
+
+type Claims struct {
+	Issuer         string `json:"iss,omitempty"`
+	Subject        string `json:"sub,omitempty"`
+	Audience       string `json:"aud,omitempty"`
+	ExpirationTime int64  `json:"exp,omitempty"`
+	NotBefore      int64  `json:"nbf,omitempty"`
+	IssuedAt       int64  `json:"iat,omitempty"`
+	ID             string `json:"jti,omitempty"`
+}
--- jwt.go
+++ jwt.go
@@ -1,68 +1,23 @@
 package jwt
 
-import (
-	"encoding/base64"
-	"errors"
-)
-
-var enc = base64.RawURLEncoding
-
 // JWT is a JSON Web Token as per the RFC 7519.
 //
 // Fields are ordered according to the RFC 7519 order.
 type JWT struct {
-	hdr            *header
-	Issuer         string `json:"iss,omitempty"`
-	Subject        string `json:"sub,omitempty"`
-	Audience       string `json:"aud,omitempty"`
-	ExpirationTime int64  `json:"exp,omitempty"`
-	NotBefore      int64  `json:"nbf,omitempty"`
-	IssuedAt       int64  `json:"iat,omitempty"`
-	ID             string `json:"jti,omitempty"`
+	Header `json:"-"`
+	*Claims
 }
 
 var (
-	// ErrMalformed indicates a token doesn't have
-	// a valid format, as per the RFC 7519.
-	ErrMalformed = errors.New("jwt: malformed token")
+	_ Token = &JWT{}                 // compile-time check of Token interface by JWT
+	_ Token = &struct{ JWT }{JWT{}}  // compile-time check of Token interface by embedded JWT
+	_ Token = struct{ *JWT }{&JWT{}} // compile-time check of Token interface by embedded JWT pointer
 )
 
-// Algorithm returns the JWT's header's algorithm.
-func (jot *JWT) Algorithm() string {
-	return jot.header().Algorithm
-}
-
-// ContentType returns the JWT's header's content type.
-func (jot *JWT) ContentType() string {
-	return jot.header().ContentType
-}
-
-// KeyID returns the JWT's header's key ID.
-func (jot *JWT) KeyID() string {
-	return jot.header().KeyID
-}
-
-// SetAlgorithm sets the algorithm a JWT uses to be signed.
-func (jot *JWT) SetAlgorithm(s Signer) {
-	jot.header().Algorithm = s.String()
-}
-
-// SetContentType sets the JWT's header's content type.
-//
-// This is useful if a type implements the Marshaler and the Unmarshaler
-// types in order to use JWE instead of JWS for signing and verifying.
-func (jot *JWT) SetContentType(cty string) {
-	jot.header().ContentType = cty
-}
-
-// SetKeyID sets the key ID assigned to a JWT.
-func (jot *JWT) SetKeyID(kid string) {
-	jot.header().KeyID = kid
-}
-
-// Type returns the JWT's header's type.
-func (jot *JWT) Type() string {
-	return jot.header().Type
+// HeaderAddr returns the JWT header's address.
+// This is needed in order to implement the Token interface.
+func (jot *JWT) HeaderAddr() *Header {
+	return &jot.Header
 }
 
 // Validate validates claims and header fields.
@@ -73,17 +28,4 @@ func (jot *JWT) Validate(validators ...ValidatorFunc) error {
 		}
 	}
 	return nil
-}
-
-func (jot *JWT) header() *header {
-	if jot.hdr == nil {
-		jot.hdr = &header{
-			Type: "JWT",
-		}
-	}
-	return jot.hdr
-}
-
-func (jot *JWT) setHeader(hdr *header) {
-	jot.hdr = hdr
 }
