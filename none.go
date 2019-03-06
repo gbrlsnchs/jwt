e7ebed6329f08e124a5e62f54fa7768ada869027
Gabriel Sanches <g@lonemaker.com>
2019-03-06 14:34:34 -0300

    Remove "NewNone" function

--- none.go
+++ none.go
@@ -2,13 +2,6 @@ package jwt
 
 type None struct{}
 
-// None returns a Signer that
-// bypasses signing and validating,
-// thus implementing the "none" method.
-func NewNone() *None {
-	return &None{}
-}
-
 func (n *None) Sign(_ []byte) ([]byte, error) {
 	return nil, nil
 }
