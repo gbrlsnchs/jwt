595649a488728b47b126b4fdfec36e0454bac637
Gabriel Sanches <g@lonemaker.com>
2019-04-12 16:57:18 -0300

    Simplify Signer

--- signer.go
+++ signer.go
@@ -1,11 +1,12 @@
 package jwt
 
+import "fmt"
+
 // Signer is a JWT signer.
 type Signer interface {
+	fmt.Stringer
 	// Sign signs a JWT's header and payload.
 	Sign([]byte) ([]byte, error)
 	// SizeUp tries to return a signer's signature size.
 	SizeUp() (int, error)
-	// String returns the string representation of the signing method.
-	String() string
 }
