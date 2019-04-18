79a8887eee6380c496290ee30a48171db1e78e52
Gabriel Sanches <g@lonemaker.com>
2019-04-18 13:46:40 -0300

    Use test suite in HMAC tests

--- hmac_test.go
+++ hmac_test.go
@@ -4,22 +4,100 @@ import (
 	"testing"
 
 	. "github.com/gbrlsnchs/jwt/v3"
+	"github.com/stretchr/testify/suite"
 )
 
 func TestHMAC(t *testing.T) {
-	testCases := []testCase{
-		{NewHMAC(SHA256, []byte("")), NewHMAC(SHA256, []byte("")), ErrNoHMACKey, nil, nil, ErrNoHMACKey},
-		{NewHMAC(SHA256, []byte("secret")), NewHMAC(SHA256, []byte("secret")), nil, nil, nil, nil},
-		{NewHMAC(SHA256, []byte("secret")), NewHMAC(SHA256, []byte("not_secret")), nil, nil, nil, ErrHMACVerification},
-		{NewHMAC(SHA256, []byte("not_secret")), NewHMAC(SHA256, []byte("secret")), nil, nil, nil, ErrHMACVerification},
-		{NewHMAC(SHA384, []byte("")), NewHMAC(SHA384, []byte("")), ErrNoHMACKey, nil, nil, ErrNoHMACKey},
-		{NewHMAC(SHA384, []byte("secret")), NewHMAC(SHA384, []byte("secret")), nil, nil, nil, nil},
-		{NewHMAC(SHA384, []byte("secret")), NewHMAC(SHA384, []byte("not_secret")), nil, nil, nil, ErrHMACVerification},
-		{NewHMAC(SHA384, []byte("not_secret")), NewHMAC(SHA384, []byte("secret")), nil, nil, nil, ErrHMACVerification},
-		{NewHMAC(SHA512, []byte("")), NewHMAC(SHA512, []byte("")), ErrNoHMACKey, nil, nil, ErrNoHMACKey},
-		{NewHMAC(SHA512, []byte("secret")), NewHMAC(SHA512, []byte("secret")), nil, nil, nil, nil},
-		{NewHMAC(SHA512, []byte("secret")), NewHMAC(SHA512, []byte("not_secret")), nil, nil, nil, ErrHMACVerification},
-		{NewHMAC(SHA512, []byte("not_secret")), NewHMAC(SHA512, []byte("secret")), nil, nil, nil, ErrHMACVerification},
+	testCases := []payloadTestSuite{
+		// HS256
+		{
+			s:       NewHMAC(SHA256, []byte("")),
+			vr:      NewHMAC(SHA256, []byte("")),
+			signErr: ErrNoHMACKey,
+		},
+		{
+			s:  NewHMAC(SHA256, []byte("secret")),
+			vr: NewHMAC(SHA256, []byte("secret")),
+		},
+		{
+			s:         NewHMAC(SHA256, []byte("secret")),
+			vr:        NewHMAC(SHA256, []byte("not_secret")),
+			decodeErr: ErrHMACVerification,
+		},
+		{
+			s:         NewHMAC(SHA256, []byte("not_secret")),
+			vr:        NewHMAC(SHA256, []byte("secret")),
+			decodeErr: ErrHMACVerification,
+		},
+		{
+			s:         NewHMAC(SHA384, []byte("secret")),
+			vr:        NewHMAC(SHA256, []byte("secret")),
+			decodeErr: ErrAlgValidation,
+		},
+		/*
+			TODO(gbrlsnchs):
+			s: HS256, vr: None/RS256/ES256/Ed25519
+		*/
+		// HS384
+		{
+			s:       NewHMAC(SHA384, []byte("")),
+			vr:      NewHMAC(SHA384, []byte("")),
+			signErr: ErrNoHMACKey,
+		},
+		{
+			s:  NewHMAC(SHA384, []byte("secret")),
+			vr: NewHMAC(SHA384, []byte("secret")),
+		},
+		{
+			s:         NewHMAC(SHA384, []byte("secret")),
+			vr:        NewHMAC(SHA384, []byte("not_secret")),
+			decodeErr: ErrHMACVerification,
+		},
+		{
+			s:         NewHMAC(SHA384, []byte("not_secret")),
+			vr:        NewHMAC(SHA384, []byte("secret")),
+			decodeErr: ErrHMACVerification,
+		},
+		{
+			s:         NewHMAC(SHA512, []byte("secret")),
+			vr:        NewHMAC(SHA384, []byte("secret")),
+			decodeErr: ErrAlgValidation,
+		},
+		/*
+			TODO(gbrlsnchs):
+			s: HS384, vr: None/RS384/ES384/Ed25519
+		*/
+		// HS512
+		{
+			s:       NewHMAC(SHA512, []byte("")),
+			vr:      NewHMAC(SHA512, []byte("")),
+			signErr: ErrNoHMACKey,
+		},
+		{
+			s:  NewHMAC(SHA512, []byte("secret")),
+			vr: NewHMAC(SHA512, []byte("secret")),
+		},
+		{
+			s:         NewHMAC(SHA512, []byte("secret")),
+			vr:        NewHMAC(SHA512, []byte("not_secret")),
+			decodeErr: ErrHMACVerification,
+		},
+		{
+			s:         NewHMAC(SHA512, []byte("not_secret")),
+			vr:        NewHMAC(SHA512, []byte("secret")),
+			decodeErr: ErrHMACVerification,
+		},
+		{
+			s:         NewHMAC(SHA256, []byte("secret")),
+			vr:        NewHMAC(SHA512, []byte("secret")),
+			decodeErr: ErrAlgValidation,
+		},
+		/*
+			TODO(gbrlsnchs):
+			s: HS512, vr: None/RS512/ES512/Ed25519
+		*/
 	}
-	testJWT(t, testCases)
+	for _, tc := range testCases {
+		t.Run("", func(t *testing.T) { suite.Run(t, &tc) })
+	}
 }
