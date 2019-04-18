33fe24e0e1461cdc68d0ee7897c1a392fc3099d6
Gabriel Sanches <g@lonemaker.com>
2019-04-18 13:40:42 -0300

    Remove unneeded composition in hash pool

--- pool.go
+++ pool.go
@@ -6,24 +6,20 @@ import (
 )
 
 type pool struct {
-	sp *sync.Pool
+	*sync.Pool
 }
 
 func newPool(hfunc func() hash.Hash) *pool {
 	var p pool
-	p.sp = &sync.Pool{
-		New: func() interface{} {
-			return hfunc()
-		},
-	}
+	p.Pool = &sync.Pool{New: func() interface{} { return hfunc() }}
 	return &p
 }
 
 func (p *pool) sign(payload []byte) ([]byte, error) {
-	hh := p.sp.Get().(hash.Hash)
+	hh := p.Pool.Get().(hash.Hash)
 	defer func() {
 		hh.Reset()
-		p.sp.Put(hh)
+		p.Pool.Put(hh)
 	}()
 
 	if _, err := hh.Write(payload); err != nil {
