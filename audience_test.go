d40e6949832626b3aa5aed377cc51439ddf647b5
Gabriel Sanches <g@lonemaker.com>
2019-05-06 11:36:29 -0300

    Refactor Audience tests

--- audience_test.go
+++ audience_test.go
@@ -5,64 +5,86 @@ import (
 	"reflect"
 	"testing"
 
-	. "github.com/gbrlsnchs/jwt/v3"
+	"github.com/gbrlsnchs/jwt/v3"
 )
 
 func TestAudienceMarshal(t *testing.T) {
 	testCases := []struct {
-		aud      Audience
+		aud      jwt.Audience
 		expected string
 	}{
-		{Audience{"foo"}, `"foo"`},
-		{Audience{"foo", "bar"}, `["foo","bar"]`},
+		{jwt.Audience{"foo"}, `"foo"`},
+		{jwt.Audience{"foo", "bar"}, `["foo","bar"]`},
 		{nil, `""`},
-		{Audience{}, `""`},
-		{Audience{""}, `""`},
+		{jwt.Audience{}, `""`},
+		{jwt.Audience{""}, `""`},
 	}
 	for _, tc := range testCases {
 		t.Run(tc.expected, func(t *testing.T) {
-			b, err := json.Marshal(tc.aud)
-			if err != nil {
+			var (
+				b   []byte
+				err error
+			)
+			if tc.aud != nil {
+				if b, err = tc.aud.MarshalJSON(); err != nil {
+					t.Fatal(err)
+				}
+				checkAudMarshal(t, tc.expected, b)
+			}
+			if b, err = json.Marshal(tc.aud); err != nil {
 				t.Fatal(err)
 			}
-			if want, got := tc.expected, b; want != string(got) {
-				t.Errorf("want %s, got %s", want, got)
-			}
+			checkAudMarshal(t, tc.expected, b)
 		})
 	}
 }
 
 func TestAudienceOmitempty(t *testing.T) {
-	v := struct {
-		Audience Audience `json:"aud,omitempty"`
-	}{}
-	b, err := json.Marshal(v)
-	if err != nil {
+	var (
+		b   []byte
+		err error
+		v   = struct {
+			Audience jwt.Audience `json:"aud,omitempty"`
+		}{}
+	)
+	if b, err = json.Marshal(v); err != nil {
 		t.Fatal(err)
 	}
-	if want, got := "{}", b; want != string(got) {
-		t.Errorf("want %s, got %s", want, got)
-	}
+	checkAudMarshal(t, "{}", b)
 }
 
 func TestAudienceUnmarshal(t *testing.T) {
 	testCases := []struct {
 		jstr     []byte
-		expected Audience
+		expected jwt.Audience
 	}{
-		{[]byte(`"foo"`), Audience{"foo"}},
-		{[]byte(`["foo","bar"]`), Audience{"foo", "bar"}},
-		{[]byte("[]"), Audience{}},
+		{[]byte(`"foo"`), jwt.Audience{"foo"}},
+		{[]byte(`["foo","bar"]`), jwt.Audience{"foo", "bar"}},
+		{[]byte("[]"), jwt.Audience{}},
 	}
 	for _, tc := range testCases {
 		t.Run(string(tc.jstr), func(t *testing.T) {
-			var aud Audience
+			var aud jwt.Audience
+			if err := aud.UnmarshalJSON(tc.jstr); err != nil {
+				t.Fatal(err)
+			}
+			checkAudUnmarshal(t, tc.expected, aud)
 			if err := json.Unmarshal(tc.jstr, &aud); err != nil {
 				t.Fatal(err)
 			}
-			if want, got := tc.expected, aud; !reflect.DeepEqual(want, got) {
-				t.Errorf("want %v, got %v", want, got)
-			}
+			checkAudUnmarshal(t, tc.expected, aud)
 		})
+	}
+}
+
+func checkAudMarshal(t *testing.T, want string, got []byte) {
+	if want != string(got) {
+		t.Errorf("want %q, got %q", want, got)
+	}
+}
+
+func checkAudUnmarshal(t *testing.T, want, got jwt.Audience) {
+	if !reflect.DeepEqual(want, got) {
+		t.Errorf("want %v, got %v", want, got)
 	}
 }
