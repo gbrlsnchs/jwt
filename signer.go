4a8d39c43005f331b4c35dce8ccdada132a4409e
Gabriel Sanches <g@lonemaker.com>
2019-05-09 16:04:25 -0300

    Change Signer signature

--- signer.go
+++ signer.go
@@ -5,8 +5,7 @@ import "fmt"
 // Signer is a JWT signer.
 type Signer interface {
 	fmt.Stringer
-	// Sign signs a JWT's header and payload.
 	Sign([]byte) ([]byte, error)
-	// SizeUp tries to return a signer's signature size.
-	SizeUp() (int, error)
+	Size() int
+	Valid() bool
 }
