63dfce13a7b0e35e23bd26125ad28665b5b0063e
Gabriel Sanches <g@lonemaker.com>
2019-05-09 16:19:53 -0300

    Refactor hash pool

--- hash_pool.go
+++ hash_pool.go
@@ -13,14 +13,14 @@ func newHashPool(hfunc func() hash.Hash) *hashPool {
 	return &hashPool{&sync.Pool{New: func() interface{} { return hfunc() }}}
 }
 
-func (hp *hashPool) sign(payload []byte) ([]byte, error) {
+func (hp *hashPool) sign(headerPayload []byte) ([]byte, error) {
 	hh := hp.Pool.Get().(hash.Hash)
 	defer func() {
 		hh.Reset()
 		hp.Pool.Put(hh)
 	}()
 
-	if _, err := hh.Write(payload); err != nil {
+	if _, err := hh.Write(headerPayload); err != nil {
 		return nil, err
 	}
 	return hh.Sum(nil), nil
