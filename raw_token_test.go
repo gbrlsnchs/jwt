92bf0b5f5f85f68efa9208e5bb0eb1c5f2fe4212
Gabriel Sanches <g@lonemaker.com>
2019-05-09 11:46:33 -0300

    Add tests for RawToken decoding

--- raw_token_test.go
+++ raw_token_test.go
@@ -1,6 +1,8 @@
 package jwt_test
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"reflect"
 	"testing"
 
@@ -9,11 +11,13 @@ import (
 )
 
 var (
-	validRawToken, _  = jwt.Parse([]byte(validToken))
-	validRawToken2, _ = jwt.Parse([]byte(validToken[:112]))
-	validRawToken3, _ = jwt.Parse([]byte("a.a."))
-	validRawToken4, _ = jwt.Parse([]byte("a.a.a"))
-	invalidRawToken   jwt.RawToken
+	validRawToken, _         = jwt.Parse([]byte(validToken))
+	validRawToken2, _        = jwt.Parse([]byte(validToken[:112]))
+	validRawToken3, _        = jwt.Parse([]byte("a.a."))
+	validRawToken4, _        = jwt.Parse([]byte("a.a.a"))
+	invalidJSONRawToken, _   = jwt.Parse([]byte(".."))
+	invalidBase64RawToken, _ = jwt.Parse([]byte("{}.{}."))
+	invalidRawToken, _       = jwt.Parse(nil)
 )
 
 func TestParse(t *testing.T) {
@@ -40,7 +44,39 @@ func TestParse(t *testing.T) {
 		})
 	}
 }
+
+func TestRawTokenDecode(t *testing.T) {
+	testCases := []struct {
+		r       jwt.RawToken
+		payload interface{}
+		err     error
+	}{
+		{validRawToken, new(jwt.Payload), nil},
+		{validRawToken2, new(jwt.Payload), nil},
+		{invalidRawToken, new(jwt.Payload), jwt.ErrMalformed},
+		{invalidJSONRawToken, new(jwt.Payload), (*json.SyntaxError)(nil)},
+		{invalidBase64RawToken, new(jwt.Payload), base64.CorruptInputError(0)},
+		{validRawToken, nil, (*json.InvalidUnmarshalError)(nil)},
+	}
+	for _, tc := range testCases {
+		t.Run("", func(t *testing.T) {
+			_, err := tc.r.Decode(tc.payload)
+			if want, got := tc.err, err; !checkErr(want, got) {
+				t.Errorf("want %#v, got %#v", want, got)
 			}
 		})
+	}
+}
+
+func checkErr(want, got error) bool {
+	switch err := want.(type) {
+	case *json.SyntaxError:
+		return internal.ErrorAs(got, &err)
+	case *json.InvalidUnmarshalError:
+		return internal.ErrorAs(got, &err)
+	case base64.CorruptInputError:
+		return internal.ErrorAs(got, &err)
+	default:
+		return internal.ErrorIs(got, err)
 	}
 }
