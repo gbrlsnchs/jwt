9960278e845a7a05b7444e46f87df0685ca19cc2
Gabriel Sanches <g@lonemaker.com>
2019-05-09 18:14:44 -0300

    Add tests for None

--- none_test.go
+++ none_test.go
@@ -3,28 +3,82 @@ package jwt_test
 import (
 	"testing"
 
-	. "github.com/gbrlsnchs/jwt/v3"
-	"github.com/stretchr/testify/suite"
+	"github.com/gbrlsnchs/jwt/v3"
+	"github.com/gbrlsnchs/jwt/v3/internal"
 )
 
-func TestNone(t *testing.T) {
-	testCases := []payloadTestSuite{
-		{
-			s:  new(None),
-			vr: new(None),
-		},
-		{
-			s:         new(None),
-			vr:        NewHMAC(SHA256, []byte("secret")),
-			decodeErr: ErrAlgValidation,
-		},
-		{
-			s:         NewHMAC(SHA256, []byte("secret")),
-			vr:        new(None),
-			decodeErr: ErrAlgValidation,
-		},
+var defaultNoneHeader = []byte("eyJhbGciOiJub25lIiwidHlwIjoiSldUIn0")
+
+func TestNoneSign(t *testing.T) {
+	testCases := []struct {
+		n             *jwt.None
+		headerPayload []byte
+		want          []byte
+		err           error
+	}{
+		{new(jwt.None), claims(defaultNoneHeader, defaultPayload), nil, nil},
 	}
 	for _, tc := range testCases {
-		t.Run("", func(t *testing.T) { suite.Run(t, &tc) })
+		t.Run("", func(t *testing.T) {
+			sig, err := tc.n.Sign(tc.headerPayload)
+			if want, got := tc.want, sig; string(want) != string(got) {
+				t.Errorf("\nwant %s\ngot %s", want, got)
+			}
+			if want, got := tc.err, err; !internal.ErrorIs(got, want) {
+				t.Errorf("want %#v, got %#v", want, got)
+			}
+		})
+	}
+}
+
+func TestNoneSize(t *testing.T) {
+	testCases := []struct {
+		n    *jwt.None
+		want int
+	}{
+		{new(jwt.None), 0},
+	}
+	for _, tc := range testCases {
+		t.Run("", func(t *testing.T) {
+			if want, got := tc.want, tc.n.Size(); want != got {
+				t.Errorf("want %d, got %d", want, got)
+			}
+		})
+	}
+}
+
+func TestNoneString(t *testing.T) {
+	testCases := []struct {
+		n    *jwt.None
+		want string
+	}{
+		{new(jwt.None), jwt.MethodNone},
+	}
+	for _, tc := range testCases {
+		t.Run("", func(t *testing.T) {
+			if want, got := tc.want, tc.n.String(); want != got {
+				t.Errorf("want %s, got %s", want, got)
+			}
+		})
+	}
+}
+
+func TestNoneVerify(t *testing.T) {
+	testCases := []struct {
+		n             *jwt.None
+		headerPayload []byte
+		sig           []byte
+		err           error
+	}{
+		{new(jwt.None), claims(defaultPayload, defaultNoneHeader), nil, nil},
+		{new(jwt.None), nil, nil, nil},
+	}
+	for _, tc := range testCases {
+		t.Run("", func(t *testing.T) {
+			err := tc.n.Verify(tc.headerPayload, tc.sig)
+			if want, got := tc.err, err; !internal.ErrorIs(got, want) {
+				t.Errorf("want %#v, got %#v", want, got)
+			}
+		})
 	}
 }
