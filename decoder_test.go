93bdb5543835f933fc4c45b936cee9eed31e34f6
Gabriel Sanches <g@lonemaker.com>
2019-05-09 13:31:42 -0300

    Refactor Decoder tests

--- decoder_test.go
+++ decoder_test.go
@@ -6,75 +6,62 @@ import (
 	"testing"
 
 	"github.com/gbrlsnchs/jwt/v3"
-	"github.com/gbrlsnchs/jwt/v3/internal"
 )
 
-// Token extracted from https://jwt.io.
+// Token extracted from https://jwt.io:
+//
+//	[Header]
+//	{
+//	  "alg": "HS256",
+//	  "typ": "JWT"
+//	}
+//
+//	[Payload]
+//	{
+//	  "sub": "1234567890",
+//	  "name": "John Doe",
+//	  "iat": 1516239022
+//	}
 const validToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9." +
 	"eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ." +
 	"SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c"
 
-func TestDecode(t *testing.T) {
+var (
+	hs256 = jwt.NewHMAC(jwt.SHA256, []byte("your-256-bit-secret"))
+	hs384 = jwt.NewHMAC(jwt.SHA384, []byte("your-384-bit-secret"))
+	hs512 = jwt.NewHMAC(jwt.SHA512, []byte("your-512-bit-secret"))
+)
+
+func TestDecoderDecode(t *testing.T) {
 	testCases := []struct {
-		token            []byte
-		p                *jwt.Payload
-		jsonSyntaxErr    bool
-		jsonUnmarshalErr bool
-		base64InputErr   bool
-		err              error
+		d       *jwt.Decoder
+		payload jwt.Validator
+		funcs   []jwt.ValidatorFunc
+		err     error
 	}{
-		{
-			token: []byte(validToken),
-			p:     new(jwt.Payload),
-		},
-		{
-			token: []byte(validToken[:112]),
-			p:     new(jwt.Payload),
-			err:   jwt.ErrHMACVerification,
-		},
-		{
-			token: []byte(validToken[:111]),
-			p:     new(jwt.Payload),
-			err:   jwt.ErrMalformed,
-		},
-		{
-			token:         []byte(".."),
-			p:             new(jwt.Payload),
-			jsonSyntaxErr: true,
-		},
-		{
-			token:          []byte("{}.{}."),
-			p:              new(jwt.Payload),
-			base64InputErr: true,
-		},
-		{
-			token:            []byte(validToken),
-			p:                nil,
-			jsonUnmarshalErr: true,
-		},
+		{jwt.NewDecoder([]byte(validToken), hs256), new(jwt.Payload), nil, nil},
+		{jwt.NewDecoder([]byte(validToken), hs384), new(jwt.Payload), nil, jwt.ErrAlgValidation},
+		{jwt.NewDecoder([]byte(validToken), hs512), new(jwt.Payload), nil, jwt.ErrAlgValidation},
+		{jwt.NewDecoder([]byte(validToken[:112]), hs256), new(jwt.Payload), nil, jwt.ErrHMACVerification},
+		{jwt.NewDecoder([]byte(validToken[:112]), hs384), new(jwt.Payload), nil, jwt.ErrAlgValidation},
+		{jwt.NewDecoder([]byte(validToken[:112]), hs512), new(jwt.Payload), nil, jwt.ErrAlgValidation},
+		{jwt.NewDecoder([]byte(validToken[:111]), hs256), new(jwt.Payload), nil, jwt.ErrMalformed},
+		{jwt.NewDecoder([]byte(validToken[:111]), hs384), new(jwt.Payload), nil, jwt.ErrMalformed},
+		{jwt.NewDecoder([]byte(validToken[:111]), hs512), new(jwt.Payload), nil, jwt.ErrMalformed},
+		{jwt.NewDecoder([]byte(".."), hs256), new(jwt.Payload), nil, (*json.SyntaxError)(nil)},
+		{jwt.NewDecoder([]byte(".."), hs384), new(jwt.Payload), nil, (*json.SyntaxError)(nil)},
+		{jwt.NewDecoder([]byte(".."), hs512), new(jwt.Payload), nil, (*json.SyntaxError)(nil)},
+		{jwt.NewDecoder([]byte("{}.{}."), hs256), new(jwt.Payload), nil, base64.CorruptInputError(0)},
+		{jwt.NewDecoder([]byte("{}.{}."), hs384), new(jwt.Payload), nil, base64.CorruptInputError(0)},
+		{jwt.NewDecoder([]byte("{}.{}."), hs512), new(jwt.Payload), nil, base64.CorruptInputError(0)},
+		{jwt.NewDecoder([]byte(validToken), hs256), nil, nil, (*json.InvalidUnmarshalError)(nil)},
+		{jwt.NewDecoder([]byte(validToken), hs384), nil, nil, (*json.InvalidUnmarshalError)(nil)},
+		{jwt.NewDecoder([]byte(validToken), hs512), nil, nil, (*json.InvalidUnmarshalError)(nil)},
 	}
 	for _, tc := range testCases {
 		t.Run("", func(t *testing.T) {
-			var (
-				err = jwt.NewDecoder(tc.token,
-					jwt.NewHMAC(jwt.SHA256, []byte("your-256-bit-secret"))).Decode(tc.p)
-				syntaxErr    *json.SyntaxError
-				unmarshalErr *json.InvalidUnmarshalError
-				inputErr     base64.CorruptInputError
-			)
-			if want, got := tc.jsonSyntaxErr, internal.ErrorAs(err, &syntaxErr); want != got {
-				t.Fatalf("want %t, got %t: (%T) %v", want, got, err, err)
-			}
-			if want, got := tc.jsonUnmarshalErr, internal.ErrorAs(err, &unmarshalErr); want != got {
-				t.Fatalf("want %t, got %t: (%T) %v", want, got, err, err)
-			}
-			if want, got := tc.base64InputErr, internal.ErrorAs(err, &inputErr); want != got {
-				t.Fatalf("want %t, got %t: (%T) %v", want, got, err, err)
-			}
-			if tc.jsonSyntaxErr || tc.jsonUnmarshalErr || tc.base64InputErr {
-				return
-			}
-			if want, got := tc.err, err; !internal.ErrorIs(err, tc.err) {
+			err := tc.d.Decode(tc.payload)
+			if want, got := tc.err, err; !checkErr(tc.err, err) {
 				t.Errorf("want %v, got %v", want, got)
 			}
 		})
