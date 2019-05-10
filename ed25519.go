5a82890205300b757ee7eb3333343cb960f2206d
Gabriel Sanches <g@lonemaker.com>
2019-05-10 14:41:39 -0300

    Wrap Ed25519 using internal package
    From go1.13 onwards, the "ed25519" package will be part of the standard
    library, so in order to preserve compatibility with go1.9.7, go1.10.3,
    go1.11 and go1.12, a wrapper that uses build constraints has been
    created. Therefore, Go versions lower than 1.13 will use
    "golang.org/x/crypto/ed25519" instead.

--- ed25519.go
+++ ed25519.go
@@ -1,64 +1,33 @@
 package jwt
 
-import (
-	"errors"
-
-	"github.com/gbrlsnchs/jwt/v3/internal"
-	"golang.org/x/crypto/ed25519"
-)
+import "github.com/gbrlsnchs/jwt/v3/internal"
 
 var (
 	// ErrEd25519PrivKey is the error for trying to sign a JWT with a nil private key.
-	ErrEd25519PrivKey = errors.New("jwt: Ed25519 private key is nil")
+	ErrEd25519PrivKey = internal.ErrEd25519PrivKey
 	// ErrEd25519PubKey is the error for trying to verify a JWT with a nil public key.
-	ErrEd25519PubKey = errors.New("jwt: Ed25519 public key is nil")
+	ErrEd25519PubKey = internal.ErrEd25519PubKey
 	// ErrEd25519Verification is the error for when verification with Ed25519 fails.
-	ErrEd25519Verification = errors.New("jwt: Ed25519 verification failed")
+	ErrEd25519Verification = internal.ErrEd25519Verification
 
 	_ Signer   = new(Ed25519)
 	_ Verifier = new(Ed25519)
+
+	// NewEd25519 creates a new Ed25519 signing
+	// method with the SHA-512 hashing function.
+	NewEd25519 = internal.NewEd25519
 )
 
 // Ed25519 is a signing method that uses EdDSA
 // with a SHA-512 hashing function to sign tokens.
 type Ed25519 struct {
-	priv ed25519.PrivateKey
-	pub  ed25519.PublicKey
-}
-
-// NewEd25519 creates a new Ed25519 signing method with the SHA-512 hashing function.
-func NewEd25519(priv ed25519.PrivateKey, pub ed25519.PublicKey) *Ed25519 {
-	return &Ed25519{priv: priv, pub: pub}
-}
-
-// Sign signs a payload and returns the signature.
-func (e *Ed25519) Sign(payload []byte) ([]byte, error) {
-	if e.priv == nil {
-		return nil, ErrEd25519PrivKey
-	}
-	return ed25519.Sign(e.priv, payload), nil
-}
-
-// Size returns the signature byte size.
-func (e *Ed25519) Size() int {
-	return ed25519.SignatureSize
+	// There is a compatibility layer in order to make this
+	// work between go1.9.7, go1.10.3, go1.11, go1.12 and go1.13+,
+	// where package "ed25519" will be included in package "crypto".
+	*internal.Ed25519
 }
 
 // String returns the signing method name.
 func (e *Ed25519) String() string {
 	return MethodEd25519
-}
-
-// Verify verifies a payload and a signature.
-func (e *Ed25519) Verify(payload, sig []byte) (err error) {
-	if e.pub == nil {
-		return ErrEd25519PubKey
-	}
-	if sig, err = internal.DecodeToBytes(sig); err != nil {
-		return err
-	}
-	if !ed25519.Verify(e.pub, payload, sig) {
-		return ErrEd25519Verification
-	}
-	return nil
 }
---
+++ internal/ed25519_go1_12.go
@@ -0,0 +1,57 @@
+// +build !go1.13
+
+package internal
+
+import (
+	"errors"
+
+	"golang.org/x/crypto/ed25519"
+)
+
+var (
+	// ErrEd25519PrivKey is the error for trying to sign a JWT with a nil private key.
+	ErrEd25519PrivKey = errors.New("jwt: Ed25519 private key is nil")
+	// ErrEd25519PubKey is the error for trying to verify a JWT with a nil public key.
+	ErrEd25519PubKey = errors.New("jwt: Ed25519 public key is nil")
+	// ErrEd25519Verification is the error for when verification with Ed25519 fails.
+	ErrEd25519Verification = errors.New("jwt: Ed25519 verification failed")
+)
+
+// Ed25519 is a signing method that uses EdDSA
+// with a SHA-512 hashing function to sign tokens.
+type Ed25519 struct {
+	priv ed25519.PrivateKey
+	pub  ed25519.PublicKey
+}
+
+// NewEd25519 creates a new Ed25519 signing method with the SHA-512 hashing function.
+func NewEd25519(priv ed25519.PrivateKey, pub ed25519.PublicKey) *Ed25519 {
+	return &Ed25519{priv: priv, pub: pub}
+}
+
+// Sign signs a payload and returns the signature.
+func (e *Ed25519) Sign(headerPayload []byte) ([]byte, error) {
+	if e.priv == nil {
+		return nil, ErrEd25519PrivKey
+	}
+	return ed25519.Sign(e.priv, payload), nil
+}
+
+// Size returns the signature byte size.
+func (e *Ed25519) Size() int {
+	return ed25519.SignatureSize
+}
+
+// Verify verifies a payload and a signature.
+func (e *Ed25519) Verify(payload, sig []byte) (err error) {
+	if e.pub == nil {
+		return ErrEd25519PubKey
+	}
+	if sig, err = internal.DecodeToBytes(sig); err != nil {
+		return err
+	}
+	if !ed25519.Verify(e.pub, payload, sig) {
+		return ErrEd25519Verification
+	}
+	return nil
+}
