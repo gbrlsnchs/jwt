4eceeab1e3be902c5dfc6d0c2f0169a53b96a1c8
Gabriel Sanches <gabriel@gsr.dev>
2019-07-01 16:38:18 -0300

    header: remove validation method
    A header will always be validated when using the Verify function. Thus,
    a method for validating a Header separately is not needed.

--- header.go
+++ header.go
@@ -1,10 +1,5 @@
 package jwt
 
-import "github.com/gbrlsnchs/jwt/v3/internal"
-
-// ErrAlgValidation indicates an incoming JWT's "alg" field mismatches the Validator's.
-var ErrAlgValidation = internal.NewError(`"alg" field mismatch`)
-
 // Header is a JOSE header narrowed down to the JWT specification from RFC 7519.
 //
 // Parameters are ordered according to the RFC 7515.
@@ -13,12 +8,4 @@ type Header struct {
 	KeyID       string `json:"kid,omitempty"`
 	Type        string `json:"typ,omitempty"`
 	ContentType string `json:"cty,omitempty"`
-}
-
-// Validate checks whether the incoming header contains the correct "alg" field.
-func (h Header) Validate(vr Verifier) error {
-	if h.Algorithm != vr.String() {
-		return internal.Errorf("jwt: unexpected algorithm %q: %w", h.Algorithm, ErrAlgValidation)
-	}
-	return nil
 }
