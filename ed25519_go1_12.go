cd14621f951c81690f913f571f4dc91dbf4be74c
Gabriel Sanches <gabriel@gsr.dev>
2019-07-01 18:48:47 -0300

    ed25519: refactor methods to implement Algorithm
    Also, there is no need to keep the internal implementation.

--- ed25519_go1_12.go
+++ ed25519_go1_12.go
@@ -1,33 +1,61 @@
+// +build !go1.13
+
 package jwt
 
-import "github.com/gbrlsnchs/jwt/v3/internal"
+import (
+	"github.com/gbrlsnchs/jwt/v3/internal"
+	"golang.org/x/crypto/ed25519"
+)
 
 var (
 	// ErrEd25519PrivKey is the error for trying to sign a JWT with a nil private key.
-	ErrEd25519PrivKey = internal.ErrEd25519PrivKey
+	ErrEd25519PrivKey = internal.NewError("jwt: edDSA private key is nil")
 	// ErrEd25519PubKey is the error for trying to verify a JWT with a nil public key.
-	ErrEd25519PubKey = internal.ErrEd25519PubKey
-	// ErrEd25519Verification is the error for when verification with Ed25519 fails.
-	ErrEd25519Verification = internal.ErrEd25519Verification
+	ErrEd25519PubKey = internal.NewError("jwt: edDSA public key is nil")
+	// ErrEd25519Verification is the error for when verification with edDSA fails.
+	ErrEd25519Verification = internal.NewError("jwt: edDSA verification failed")
 
-	_ Signer   = new(Ed25519)
-	_ Verifier = new(Ed25519)
-
-	// NewEd25519 creates a new Ed25519 signing
-	// method with the SHA-512 hashing function.
-	NewEd25519 = internal.NewEd25519
+	_ Algorithm = new(edDSA)
 )
 
-// Ed25519 is a signing method that uses EdDSA
-// with a SHA-512 hashing function to sign tokens.
-type Ed25519 struct {
-	// There is a compatibility layer in order to make this
-	// work between go1.9.7, go1.10.3, go1.11, go1.12 and go1.13+,
-	// where package "ed25519" will be included in package "crypto".
-	*internal.Ed25519
+type edDSA struct {
+	priv ed25519.PrivateKey
+	pub  ed25519.PublicKey
 }
 
-// String returns the signing method name.
-func (e *Ed25519) String() string {
-	return MethodEd25519
+// NewEd25519 creates a new algorithm using EdDSA and SHA-512.
+func NewEd25519(priv ed25519.PrivateKey, pub ed25519.PublicKey) Algorithm {
+	return &edDSA{priv: priv, pub: pub}
+}
+
+// Name returns the algorithm's name.
+func Name() string {
+	return "Ed25519"
+}
+
+// Sign signs headerPayload using the Ed25519 algorithm.
+func (e *edDSA) Sign(headerPayload []byte) ([]byte, error) {
+	if e.priv == nil {
+		return nil, ErrEd25519PrivKey
+	}
+	return ed25519.Sign(e.priv, headerPayload), nil
+}
+
+// Size returns the signature byte size.
+func (e *edDSA) Size() int {
+	return ed25519.SignatureSize
+}
+
+// Verify verifies a payload and a signature.
+func (e *edDSA) Verify(payload, sig []byte) (err error) {
+	if e.pub == nil {
+		return ErrEd25519PubKey
+	}
+	if sig, err = DecodeToBytes(sig); err != nil {
+		return err
+	}
+	if !ed25519.Verify(e.pub, payload, sig) {
+		return ErrEd25519Verification
+	}
+	return nil
 }
