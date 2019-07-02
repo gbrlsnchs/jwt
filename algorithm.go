243b50b3ca2bf03b6517a67f30796a46fcd01802
Gabriel Sanches <gabriel@gsr.dev>
2019-07-01 21:19:23 -0300

    algorithm: load crypto hashing functions

--- algorithm.go
+++ algorithm.go
@@ -1,5 +1,11 @@
 package jwt
 
+import (
+	// Load all hashing functions needed.
+	_ "crypto/sha256"
+	_ "crypto/sha512"
+)
+
 // Algorithm is an algorithm for both signing and verifying a JWT.
 type Algorithm interface {
 	Name() string
