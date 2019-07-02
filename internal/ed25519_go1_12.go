b44d683ae4cf6096b6c08fe71be85f3e6d52fdef
Gabriel Sanches <gabriel@gsr.dev>
2019-07-02 01:36:36 -0300

    internal: add description to Ed25519 function

--- internal/ed25519_go1_12.go
+++ internal/ed25519_go1_12.go
@@ -8,6 +8,7 @@ import (
 	"golang.org/x/crypto/ed25519"
 )
 
+// GenerateEd25519Keys generates a pair of keys for testing purposes.
 func GenerateEd25519Keys() (ed25519.PrivateKey, ed25519.PublicKey) {
 	pub, priv, err := ed25519.GenerateKey(rand.Reader)
 	if err != nil {
