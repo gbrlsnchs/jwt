afd94f033df3bb04858cb0568d01c77e55a42b11
Gabriel Sanches <gabriel@gsr.dev>
2019-07-01 21:53:41 -0300

    raw_token: rewrite tests with remaining methods

--- raw_token_test.go
+++ raw_token_test.go
@@ -1,82 +1,47 @@
 package jwt_test
 
 import (
-	"encoding/base64"
-	"encoding/json"
 	"reflect"
 	"testing"
 
 	"github.com/gbrlsnchs/jwt/v3"
-	"github.com/gbrlsnchs/jwt/v3/internal"
 )
 
 var (
-	validRawToken, _         = jwt.Parse([]byte(validToken))
-	validRawToken2, _        = jwt.Parse([]byte(validToken[:112]))
-	validRawToken3, _        = jwt.Parse([]byte("a.a."))
-	validRawToken4, _        = jwt.Parse([]byte("a.a.a"))
-	invalidJSONRawToken, _   = jwt.Parse([]byte(".."))
-	invalidBase64RawToken, _ = jwt.Parse([]byte("{}.{}."))
-	invalidRawToken, _       = jwt.Parse(nil)
+	testToken = []byte(
+		"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9." +
+			"eyJzdHJpbmciOiJmb29iYXIiLCJpbnQiOjEzMzcsImlhdCI6MTUxNjIzOTAyMn0." +
+			"bVYo9Q0lGouCj1y9zFY17bfxQaRUuM6wtpnIy0m4uD0",
+	)
+	testRaw, _ = jwt.Verify(jwt.NewHS256([]byte("secret")), testToken)
 )
-
-func TestParse(t *testing.T) {
-	testCases := []struct {
-		token []byte
-		want  jwt.RawToken
-		err   error
-	}{
-		{[]byte(validToken), validRawToken, nil},
-		{[]byte(validToken[:112]), validRawToken2, nil},
-		{[]byte(validToken[:111]), invalidRawToken, jwt.ErrMalformed},
-		{[]byte("a.a."), validRawToken3, nil},  // parsable, not valid
-		{[]byte("a.a.a"), validRawToken4, nil}, // parsable, not valid
-	}
-	for _, tc := range testCases {
-		t.Run("", func(t *testing.T) {
-			rt, err := jwt.Parse(tc.token)
-			if want, got := tc.want, rt; !reflect.DeepEqual(want, got) {
-				t.Errorf("want %v, got %v", want, got)
-			}
-			if want, got := tc.err, err; !internal.ErrorIs(got, want) {
-				t.Fatalf("want %v, got %v", want, got)
-			}
-		})
-	}
-}
 
 func TestRawTokenDecode(t *testing.T) {
 	testCases := []struct {
-		r       jwt.RawToken
-		payload interface{}
-		err     error
+		raw         jwt.RawToken
+		wantPayload testPayload
 	}{
-		{validRawToken, new(jwt.Payload), nil},
-		{validRawToken2, new(jwt.Payload), nil},
-		{invalidRawToken, new(jwt.Payload), jwt.ErrMalformed},
-		{invalidJSONRawToken, new(jwt.Payload), (*json.SyntaxError)(nil)},
-		{invalidBase64RawToken, new(jwt.Payload), base64.CorruptInputError(0)},
-		{validRawToken, nil, (*json.InvalidUnmarshalError)(nil)},
+		{
+			raw: testRaw,
+			wantPayload: testPayload{
+				String: "foobar",
+				Int:    1337,
+				Payload: jwt.Payload{
+					IssuedAt: 1516239022,
+				},
+			},
+		},
 	}
 	for _, tc := range testCases {
 		t.Run("", func(t *testing.T) {
-			_, err := tc.r.Decode(tc.payload)
-			if want, got := tc.err, err; !checkErr(want, got) {
-				t.Errorf("want %#v, got %#v", want, got)
+			var payload testPayload
+			err := tc.raw.Decode(&payload)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if want, got := tc.wantPayload, payload; !reflect.DeepEqual(got, want) {
+				t.Errorf("want %#+v, got %#+v", want, got)
 			}
 		})
-	}
-}
-
-func checkErr(want, got error) bool {
-	switch err := want.(type) {
-	case *json.SyntaxError:
-		return internal.ErrorAs(got, &err)
-	case *json.InvalidUnmarshalError:
-		return internal.ErrorAs(got, &err)
-	case base64.CorruptInputError:
-		return internal.ErrorAs(got, &err)
-	default:
-		return internal.ErrorIs(got, err)
 	}
 }
