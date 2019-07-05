85007ae9f641ea52820d819dd0fba7c01bec9391
Gabriel Sanches <gabriel@gsr.dev>
2019-07-05 13:57:47 -0300

    payload: use Time type instead of int64

--- payload.go
+++ payload.go
@@ -5,8 +5,8 @@ type Payload struct {
 	Issuer         string   `json:"iss,omitempty"`
 	Subject        string   `json:"sub,omitempty"`
 	Audience       Audience `json:"aud,omitempty"`
-	ExpirationTime int64    `json:"exp,omitempty"`
-	NotBefore      int64    `json:"nbf,omitempty"`
-	IssuedAt       int64    `json:"iat,omitempty"`
+	ExpirationTime Time     `json:"exp,omitempty"`
+	NotBefore      Time     `json:"nbf,omitempty"`
+	IssuedAt       Time     `json:"iat,omitempty"`
 	JWTID          string   `json:"jti,omitempty"`
 }
