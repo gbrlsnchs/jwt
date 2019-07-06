85f8365fa0d96df0108008251199632e08bfc33e
Gabriel Sanches <gabriel@gsr.dev>
2019-07-06 19:22:50 -0300

    payload: use pointer to Time instead of value
    This fix numeric dates not being omitted.

--- payload.go
+++ payload.go
@@ -5,8 +5,8 @@ type Payload struct {
 	Issuer         string   `json:"iss,omitempty"`
 	Subject        string   `json:"sub,omitempty"`
 	Audience       Audience `json:"aud,omitempty"`
-	ExpirationTime Time     `json:"exp,omitempty"`
-	NotBefore      Time     `json:"nbf,omitempty"`
-	IssuedAt       Time     `json:"iat,omitempty"`
+	ExpirationTime *Time    `json:"exp,omitempty"`
+	NotBefore      *Time    `json:"nbf,omitempty"`
+	IssuedAt       *Time    `json:"iat,omitempty"`
 	JWTID          string   `json:"jti,omitempty"`
 }
