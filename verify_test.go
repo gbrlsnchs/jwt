2128ca725db994f9adaffaf21c7f5147a867d033
Gabriel Sanches <gabriel@gsr.dev>
2019-07-28 01:43:24 -0300

    verify: add tests for ValidatePayload

--- verify_test.go
+++ verify_test.go
@@ -3,6 +3,7 @@ package jwt_test
 import (
 	"reflect"
 	"testing"
+	"time"
 
 	"github.com/gbrlsnchs/jwt/v3"
 )
@@ -784,6 +785,76 @@ func TestVerify(t *testing.T) {
 						t.Errorf("want %#+v, got %#+v", want, got)
 					}
 				})
+			}
+		})
+	}
+}
+
+func TestValidatePayload(t *testing.T) {
+	now := time.Now()
+	testCases := []struct {
+		pl  *jwt.Payload
+		vds []jwt.Validator
+		err error
+	}{
+		{
+			pl: &jwt.Payload{
+				ExpirationTime: jwt.NumericDate(now.Add(1 * time.Second)),
+			},
+			vds: []jwt.Validator{jwt.ExpirationTimeValidator(now)},
+			err: nil,
+		},
+		{
+			pl: &jwt.Payload{
+				ExpirationTime: jwt.NumericDate(now.Add(1 * time.Second)),
+			},
+			vds: []jwt.Validator{jwt.ExpirationTimeValidator(now.Add(15 * time.Second))},
+			err: jwt.ErrExpValidation,
+		},
+		{
+			pl: &jwt.Payload{
+				Subject:        "test",
+				ExpirationTime: jwt.NumericDate(now.Add(1 * time.Second)),
+			},
+			vds: []jwt.Validator{
+				jwt.SubjectValidator("test"),
+				jwt.ExpirationTimeValidator(now),
+			},
+			err: nil,
+		},
+		{
+			pl: &jwt.Payload{
+				Subject:        "foo",
+				ExpirationTime: jwt.NumericDate(now.Add(1 * time.Second)),
+			},
+			vds: []jwt.Validator{
+				jwt.SubjectValidator("bar"),
+				jwt.ExpirationTimeValidator(now),
+			},
+			err: jwt.ErrSubValidation,
+		},
+		{
+			pl: &jwt.Payload{
+				Subject:        "test",
+				ExpirationTime: jwt.NumericDate(now.Add(1 * time.Second)),
+			},
+			vds: []jwt.Validator{
+				jwt.SubjectValidator("test"),
+				jwt.ExpirationTimeValidator(now.Add(15 * time.Second)),
+			},
+			err: jwt.ErrExpValidation,
+		},
+	}
+	hs256 := jwt.NewHS256([]byte("secret"))
+	for _, tc := range testCases {
+		t.Run("", func(t *testing.T) {
+			token, err := jwt.Sign(tc.pl, hs256)
+			if err != nil {
+				t.Fatal(err)
+			}
+			_, err = jwt.Verify(token, hs256, tc.pl, jwt.ValidatePayload(tc.pl, tc.vds...))
+			if want, got := tc.err, err; got != want {
+				t.Errorf("want %v, got %v", want, got)
 			}
 		})
 	}
